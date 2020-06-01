3cba3cbd1071d54378a68ed051c02e5a26b318b4
mpstv <54762420+mpstv@users.noreply.github.com>
2020-06-01 19:49:20 +0300

    demonstrate problem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,49 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { record } from "rrweb";
+import { eventWithTime, listenerHandler } from "rrweb/typings/types";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
+    <div class="content">
+      <h1>Welcome to {{ title }}!</h1>
+
+      <div>
+        <button (click)="startRecord()">Start record</button>
+        <button (click)="stopRecord()">Stop record</button>
+      </div>
+
+      <p>
+        {{ recordingSession ? "record on" : "record off" }}
+      </p>
     </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
   `,
-  styles: []
+  styles: [],
 })
 export class AppComponent {
-  title = 'rrweb-event-after-stop-recording-problem';
+  title = "rrweb-event-after-stop-recording-problem";
+
+  recordingSession: listenerHandler;
+  events: eventWithTime[] = [];
+
+  startRecord() {
+    console.warn("start recording");
+    this.recordingSession = record({
+      emit: (event) => {
+        this.events.push(event);
+        console.log(
+          `event added! type: [${event.type}], timestamp: [${event.timestamp}], delay: [${event.delay}]`
+        );
+      },
+    });
+  }
+
+  stopRecord() {
+    if (this.recordingSession) {
+      this.recordingSession();
+      this.recordingSession = null;
+      this.events = [];
+      console.warn("stop recording");
+    }
+  }
 }
